b88d9058e55dc68cbd674e4c02a2102f7cf75775
NoaSalih <nawar2000@hotmail.com>
2022-06-16 17:21:52 +0200

    first commit

---
+++ src/main/java/zorginstellingen/ZorginstellingenApplication.java
@@ -0,0 +1,13 @@
+package zorginstellingen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZorginstellingenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZorginstellingenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zorginstellingen/ZorginstellingenApplicationTests.java
@@ -0,0 +1,13 @@
+//package zorginstellingen;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ZorginstellingenApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
